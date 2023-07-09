9dcbcc607fafb3cf92eddc1f21683a1509d76f5b
Nekolike <nigel.volkmann1994@googlemail.com>
2023-07-09 20:03:46 +0200

    Change car type service to return dto

--- src/main/kotlin/de/nekolike/kfzconfigurator/service/CarTypeService.kt
+++ src/main/kotlin/de/nekolike/kfzconfigurator/service/CarTypeService.kt
@@ -1,10 +1,24 @@
 package de.nekolike.kfzconfigurator.service
 
+import de.nekolike.kfzconfigurator.dto.CarTypeDTO
 import de.nekolike.kfzconfigurator.persistence.CarType
 import de.nekolike.kfzconfigurator.repository.CarTypeRepository
 import org.springframework.stereotype.Service
 
 @Service
 class CarTypeService(val carTypeRepository: CarTypeRepository) {
-    fun findCarTypes(): List<CarType> = carTypeRepository.findAll().toList()
+    private fun findCarTypes(): List<CarType> = carTypeRepository.findAll().toList()
+
+    fun getCarTypes(): List<CarTypeDTO> {
+        val carTypes = findCarTypes()
+        val carTypeDTOs: MutableList<CarTypeDTO> = mutableListOf()
+        for (carType in carTypes) {
+            carTypeDTOs.add(CarTypeDTO(
+                carType.carType,
+                carType.price
+            ))
+        }
+
+        return carTypeDTOs
+    }
 }
