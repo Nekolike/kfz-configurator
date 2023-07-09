642e7729926dd6438e6638692951c2edc85a77b2
Nekolike <nigel.volkmann1994@googlemail.com>
2023-07-09 20:04:04 +0200

    Add table and column informations to car type entity

--- src/main/kotlin/de/nekolike/kfzconfigurator/persistence/CarType.kt
+++ src/main/kotlin/de/nekolike/kfzconfigurator/persistence/CarType.kt
@@ -1,14 +1,18 @@
 package de.nekolike.kfzconfigurator.persistence
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "car_types")
 data class CarType(
     @Id
     val id: Long,
 
-    val type: String,
+    @Column(name = "car_type")
+    val carType: String,
 
-    val price: Long
+    val price: Int
 )
