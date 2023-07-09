4ae24597fd3f7c063d4a8c21f735586ac5dc0c69
Nekolike <nigel.volkmann1994@googlemail.com>
2023-07-09 20:04:30 +0200

    Add controller with dummy get api

--- src/main/kotlin/de/nekolike/kfzconfigurator/controller/KfzConfiguratorController.kt
+++ src/main/kotlin/de/nekolike/kfzconfigurator/controller/KfzConfiguratorController.kt
@@ -1,7 +1,14 @@
 package de.nekolike.kfzconfigurator.controller
 
-import org.springframework.stereotype.Controller
+import de.nekolike.kfzconfigurator.dto.CarTypeDTO
+import de.nekolike.kfzconfigurator.service.CarTypeService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class KfzConfiguratorController {
+@RestController
+class KfzConfiguratorController(val carTypeService: CarTypeService) {
+    @GetMapping("/")
+    fun getCarTypes(): List<CarTypeDTO> {
+        return carTypeService.getCarTypes()
+    }
 }
